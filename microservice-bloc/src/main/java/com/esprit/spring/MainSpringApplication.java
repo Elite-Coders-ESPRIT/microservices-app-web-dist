f44595ed3912320afa81a2153c3560742cf350ea
Moataz024 <prexzcod@gmail.com>
2023-12-06 17:44:17 +0100

    Before creating images

--- microservice-bloc/src/main/java/com/esprit/spring/MainSpringApplication.java
+++ microservice-bloc/src/main/java/com/esprit/spring/MainSpringApplication.java
@@ -6,15 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class MainSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainSpringApplication.class, args);
     }
-
     @Autowired
     BlocRepository blocRepository;
 
--- microservice-chambre-main/src/main/java/com/esprit/spring/MainSpringApplication.java
+++ microservice-chambre-main/src/main/java/com/esprit/spring/MainSpringApplication.java
@@ -5,9 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
+
+
 @SpringBootApplication
+@EnableEurekaClient
 public class MainSpringApplication {
 
     public static void main(String[] args) {
